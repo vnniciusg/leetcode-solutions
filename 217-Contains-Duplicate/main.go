5d97e5d7632b6b656811a2985396a33e696edaf3
vnn <vinnicius109@gmail.com>
2024-05-20 14:45:01 -0300

    add 217. Contains Duplicate

---
+++ 217-Contains-Duplicate/main.go
@@ -0,0 +1,12 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]bool)
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		}
+		m[v] = true
+	}
+	return false
+}
