ccd3c7e8643f67eb0166c0095c784e3dd738146d
vnn <vinnicius109@gmail.com>
2024-05-27 08:39:56 -0300

    add 1608-special-array-with-x-elements-greater-than-or-equal-x

--- 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X/main.go
+++ 1608-Special-Array-With-X-Elements-Greater-Than-or-Equal-X/main.go
@@ -1,24 +1,26 @@
 package main
 
+import "sort"
+
 func specialArray(nums []int) int {
 
-    sort.Slice(nums, func(i, j int) bool {
-        return nums[i] < nums[j]
-    })
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
 
-    n := len(nums)
+	n := len(nums)
 
-    for i := 1; i < n + 1; i++{
-        count := 0
-        for _, num := range nums {
-            if num >= i{
-                count++
-            }
-        }
-        if count == i {
-            return i
-        }
-    }
+	for i := 1; i < n+1; i++ {
+		count := 0
+		for _, num := range nums {
+			if num >= i {
+				count++
+			}
+		}
+		if count == i {
+			return i
+		}
+	}
 
-    return -1
+	return -1
 }
