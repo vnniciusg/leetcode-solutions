1b29cf364cb7655dba00c109a8804a39094c06e1
vnn <vinnicius109@gmail.com>
2024-08-01 08:47:39 -0300

    FEAT: add solution for 2678-number-of-senior-citizens

---
+++ 2678-number-of-senior-citizens/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (const auto& detail: details) {
+            string ageStr = detail.substr(11, 2); 
+            int age = std::stoi(ageStr); 
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
