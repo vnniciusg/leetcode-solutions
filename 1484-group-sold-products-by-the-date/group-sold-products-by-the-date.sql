e369496188754158963ff4b24fea42c5d70584a3
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-01-30 15:52:59 -0300

    [FEAT]: add solution for problemn 1483

---
+++ 1484-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    STRING_AGG(DISTINCT product, ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date ASC;
