1abeecb25a55203432bcd4d354f7f31a05332264
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-01-31 10:15:55 -0300

    [FEAT]: add solution for problem 1075

---
+++ 1075-project-employees-i/project-employees-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    project_id,
+    ROUND(AVG(experience_years), 2) AS average_years
+FROM Project p
+JOIN Employee e ON e.employee_id = p.employee_id
+GROUP BY p.project_id;
