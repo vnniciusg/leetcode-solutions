857fcc2694eb9f25d4693b8ed0f761f040152220
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-02-14 08:40:04 -0300

    [FIX]: remove size from c++ solution problem 1352

--- 1352-product-of-the-last-k-numbers/main.cpp
+++ 1352-product-of-the-last-k-numbers/main.cpp
@@ -1,36 +1,25 @@
 class ProductOfNumbers {
 public:
     vector<int> prefix_product;
-    int size;
 
     ProductOfNumbers() {
         prefix_product.push_back(1);
-        size = 0;
     }
     
     void add(int num) {
         if (num == 0) {
             prefix_product.clear();
             prefix_product.push_back(1);
-            size = 0;
         }else {
             prefix_product.push_back(prefix_product.back() * num);
-            size++;
         }
     }
     
     int getProduct(int k) {
-        if (k > size) {
+        if (k >= prefix_product.size()) {
             return 0;
         }
 
         return prefix_product.back() / prefix_product[prefix_product.size() - 1 - k];
     }
 };
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * ProductOfNumbers* obj = new ProductOfNumbers();
- * obj->add(num);
- * int param_2 = obj->getProduct(k);
- */
