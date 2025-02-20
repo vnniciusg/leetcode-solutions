8cd8e325fefc18011e7b9a392e5c3929a2b3eb18
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-02-20 08:50:32 -0300

    [FEAT]: add python solution for problem 1980

---
+++ 1980-find-unique-binary-string/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        ans = []
+        n = len(nums)
+
+        for i in range(n):
+            curr = nums[i][i]
+            ans.append("1" if curr == "0" else "0")
+
+
+        print(ans)
+        return "".join(ans)
