c6839e3a69fb33bd2a253bf3b074e6e35bf472de
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-03-03 00:42:25 -0300

    [FEAT]: add python solution for problem 2161

---
+++ 2161-partition-array-according-to-given-pivot/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        left, right = [], []
+        mid = []
+
+        for num in nums:
+            if num < pivot: 
+                left.append(num)
+            elif num > pivot: 
+                right.append(num)
+            else:
+                mid.append(num)
+
+        return left + mid + right
