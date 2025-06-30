1443889369d60e91ee8a2f0b3fa89638372a3ed8
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-06-30 20:00:01 -0300

    feat: add cpp solution for problem 594

---
+++ 594-longest-harmonious-subsequence/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int j = 0, maxLength = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i] - nums[j] > 1) {
+                j++;
+            }
+
+            if (nums[i] - nums[j] == 1) {
+                maxLength = std::max(maxLength, i - j + 1);
+            }
+        }
+
+        return maxLength;
+    }
+};
